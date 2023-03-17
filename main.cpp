a6c868424976c292faae5e9aa2c1dda082237850
Schmidt Anton <schmidt.ajax@yandex.ru>
2023-03-17 21:52:07 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
 #include <cassert>
+#include <unordered_map>
 
 using namespace std;
 
 class Trie {
+  struct Node {
+    unordered_map<char, Node*> children;
+    bool is_word = false;
+
+    Node* findChild(string_view& prefix) {
+      Node* node = this;
+      while (!prefix.empty() && node->children.count(prefix.front())) {
+        node = node->children.at(prefix.front());
+        prefix.remove_prefix(1);
+      }
+      return node;
+    }
+  };
+
+  Node root_;
+
  public:
-  Trie() {
-    //FIXME
-  }
+  Trie() {}
 
   void insert(string word) {
-    //FIXME
+    string_view prefix{word};
+    Node* node = root_.findChild(prefix);
+    while (!prefix.empty()) {
+      Node* child = new Node;
+      node->children[prefix.front()] = child;
+      prefix.remove_prefix(1);
+      node = child;
+    }
+    node->is_word = true;
   }
 
   bool search(string word) {
-    //FIXME
-    return false;
+    string_view prefix{word};
+    Node* node = root_.findChild(prefix);
+    return prefix.empty() && node->is_word;
   }
 
   bool startsWith(string prefix) {
-    //FIXME
-    return false;
+    string_view p{prefix};
+    Node* node = root_.findChild(p);
+    return p.empty();
   }
 };
 
